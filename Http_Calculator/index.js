4bb020725907372d9567217d0f9d267ed0e7e6f4
DevanshRaheja <80957882+DevanshRaheja@users.noreply.github.com>
2024-02-05 16:45:02 +0530

    More Convenient and Understandable Code

--- Http_Calculator/index.js
+++ Http_Calculator/index.js
@@ -13,7 +13,7 @@ function mul(a, b) {
     return res
 }
 function div(a, b) {
-    let res = (b !== 0  ? (a / b) : `Divide by Zero not possible`)
+    let res = (b !== 0  ? (a / b) : `INFINITY`)
     return res
 }
 
@@ -28,25 +28,37 @@ app.get('/sum', (req, res) => {
     const a = req.query.a
     const b = req.query.b
     const ans = sum(a, b)
-    res.send(`Sum is ${ans}`)
+    res.send(`<h1>Sum is ${ans}</h1>`);
 })
 app.get('/difference', (req, res) => {
     const a = req.query.a
     const b = req.query.b
     const ans = diff(a, b)
-    res.send(`Difference is ${ans}`)
+    res.send(`<h1>Difference is ${ans}</h1>`);
 })
 app.get('/product', (req, res) => {
     const a = req.query.a
     const b = req.query.b
     const ans = mul(a, b)
-    res.send(`Product is ${ans}`)
+    res.send(`<h1>Product is ${ans}</h1>`);
 })
 app.get('/division', (req, res) => {
     const a = req.query.a
     const b = req.query.b
     const ans = div(a, b)
-    res.send(`Division is ${ans}`)
+    res.send(`<h1>Division is ${ans}</h1>`);
+})
+app.get('/', (req, res) => {
+    const a = req.query.a
+    const b = req.query.b
+    const ans1 = sum(a, b)
+    const ans2 = diff(a, b)
+    const ans3 = mul(a, b)
+    const ans4 = div(a, b)
+    res.send(`<h1>Sum is ${ans1}</h1>
+    <h1>Difference is ${ans2}</h1>
+    <h1>Product is ${ans3}</h1>
+    <h1>Division is ${ans4}</h1>`);
 })
 
 app.listen(3000)
