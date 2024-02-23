f64517c2d41b6922c8f494a52c7bf6ab5078b11b
DevanshRaheja <80957882+DevanshRaheja@users.noreply.github.com>
2024-02-23 14:55:00 +0530

    More into zod

--- Hospital_covid(middlewares)/Validation_using_zod.js
+++ Hospital_covid(middlewares)/Validation_using_zod.js
@@ -3,9 +3,24 @@ const zod = require('zod')      // zod is a pre-defined library used for input v
 
 const app = express();
 // const schema = zod.array(zod.number())      // performs multiple checks on kidney array
-const schema = zod.array(zod.string())      // performs multiple checks on kidney array
 
 // schema will be array of numbers
+
+/*
+{
+  email : string
+  password : atleast 8 character
+  country : IN, USA
+}
+*/
+
+const schema = zod.array(zod.string())    
+
+const schema1 = zod.object({
+  email : zod.string().email(),
+  password : zod.string().min(8),
+  country : zod.literal("IN").or(zod.literal("US"))  // Nested zod literal 
+})
 
 app.use(express.json());
 
@@ -13,12 +28,24 @@ app.post("/health-checkup", (req, res) => {
   const kidney = req.body.kidney;               // kidney is array of numbers
   const response = schema.parse(kidney)         // using zod for input validation
 
+  // if(!response.success){
+  //   res.status(403).json({
+  //     msg : "Failed to parse"
+  //   })
+  // }
+
   res.send({
     response
   });
-  console.log(response);
+  // console.log(response);
 
 });
+
+app.use(function(err, req, res, next){
+  res.status(403).json({
+    msg: "Failed to parse",
+  });
+})
 
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
