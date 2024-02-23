6c6657f3d3f8fa32b7cfa1ded0a57eaba30154ab
DevanshRaheja <80957882+DevanshRaheja@users.noreply.github.com>
2024-02-23 17:08:58 +0530

    updated code

--- Hospital_covid(middlewares)/Validation_using_zod.js
+++ Hospital_covid(middlewares)/Validation_using_zod.js
@@ -6,33 +6,19 @@ const app = express();
 
 // schema will be array of numbers
 
-/*
-{
-  email : string
-  password : atleast 8 character
-  country : IN, USA
-}
-*/
-
 const schema = zod.array(zod.string())    
-
-const schema1 = zod.object({
-  email : zod.string().email(),
-  password : zod.string().min(8),
-  country : zod.literal("IN").or(zod.literal("US"))  // Nested zod literal 
-})
 
 app.use(express.json());
 
 app.post("/health-checkup", (req, res) => {
   const kidney = req.body.kidney;               // kidney is array of numbers
-  const response = schema.parse(kidney)         // using zod for input validation
+  const response = schema.safeParse(kidney)         // using zod for input validation
 
-  // if(!response.success){
-  //   res.status(403).json({
-  //     msg : "Failed to parse"
-  //   })
-  // }
+  if(!response.success){
+    res.status(403).json({
+      msg : "Failed to parse"
+    })
+  }
 
   res.send({
     response
@@ -41,11 +27,11 @@ app.post("/health-checkup", (req, res) => {
 
 });
 
-app.use(function(err, req, res, next){
-  res.status(403).json({
-    msg: "Failed to parse",
-  });
-})
+// app.use(function(err, req, res, next){
+//   res.status(403).json({
+//     msg: "Failed to parse",
+//   });
+// })
 
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
